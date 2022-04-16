cc404c5dd523d16a02df91a5dccc625542a64700
Kayden Ahn <kayden_ahn@intuit.com>
2022-04-16 16:46:03 -0700

    update format

--- app.py
+++ app.py
@@ -4,38 +4,38 @@ from dash.dependencies import Input, Output, State
 import pickle
 import numpy as np
 
-########### Define your variables ######
-myheading1='California Housing Dataset'
+# Global variables
+myheading1 = 'California Housing Dataset'
 tabtitle = 'Cali Housing'
 sourceurl = 'https://github.com/ageron/handson-ml2/blob/master/02_end_to_end_machine_learning_project.ipynb'
-githublink = 'https://github.com/plotly-dash-apps/502-california-housing-regression'
+githublink = 'https://github.com/cahn1/502-california-housing-regression/tree/update1'
 
 
-########### open the pickle files ######
+# Load pkl files
 with open('analysis/model_components/coefs_fig.pkl', 'rb') as f:
-    coefs=pickle.load(f)
+    coefs = pickle.load(f)
 with open('analysis/model_components/r2_fig.pkl', 'rb') as f:
-    r2_fig=pickle.load(f)
+    r2_fig = pickle.load(f)
 with open('analysis/model_components/rmse_fig.pkl', 'rb') as f:
-    rmse_fig=pickle.load(f)
+    rmse_fig = pickle.load(f)
 with open('analysis/model_components/std_scaler.pkl', 'rb') as f:
-    std_scaler=pickle.load(f)
+    std_scaler = pickle.load(f)
 with open('analysis/model_components/lin_reg.pkl', 'rb') as f:
-    lin_reg=pickle.load(f)
+    lin_reg = pickle.load(f)
 
-########### Initiate the app
+# app server config
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
 app.title=tabtitle
 
-########### Set up the layout
+# Setup a layout
 app.layout = html.Div(children=[
     html.H1('California Neighborhoods'),
     html.H4('What is the Median Home Value of a Neighborhood?'),
     html.H6('Features of Neighborhood:'),
 
-    ### Prediction Block
+    # Prediction Block
     html.Div(children=[
 
         html.Div([
@@ -88,7 +88,7 @@ app.layout = html.Div(children=[
                     html.Div(id='Results')
                 ], className='four columns')
             ], className='twelve columns'),
-        ### Evaluation Block
+        # Evaluation Block
         html.Div(children=[
             html.Div(
                     [dcc.Graph(figure=r2_fig, id='r2_fig')
@@ -109,11 +109,11 @@ app.layout = html.Div(children=[
         ], className='twelve columns')
 
 
-######### Define Callback
+# callback
 @app.callback(
     Output(component_id='Results', component_property='children'),
     Input(component_id='submit-val', component_property='n_clicks'),
-    # regression inputs:
+    # Regression inputs:
     State(component_id='longitude', component_property='value'),
     State(component_id='latitude', component_property='value'),
     State(component_id='housing_median_age', component_property='value'),
@@ -125,23 +125,31 @@ app.layout = html.Div(children=[
     State(component_id='rooms_per_hhold', component_property='value'),
     State(component_id='pop_per_household', component_property='value'),
 )
-def make_prediction(clicks, longitude, latitude, housing_median_age, total_rooms,
-        population, households, median_income, income_cat,
-        rooms_per_hhold, pop_per_household):
+def make_prediction(
+        clicks,
+        longitude,
+        latitude,
+        housing_median_age,
+        total_rooms,
+        population,
+        households,
+        median_income,
+        income_cat,
+        rooms_per_hhold,
+        pop_per_household):
     if clicks==0:
         return "waiting for inputs"
     else:
-
         inputs=np.array([longitude, latitude, housing_median_age, total_rooms,
                population, households, median_income, income_cat,
                rooms_per_hhold, pop_per_household, 0, 0, 0, 0]).reshape(1, -1)
-       # note: the 4 zeroes are for missing categories=['INLAND', 'ISLAND', 'NEAR BAY','NEAR OCEAN']
-
-       # test with fake inputs
-        # fake = np.array([-122, 37, 40, 2000, 3000, 500, 3, 3, 6, 4, 0, 0, 1, 0]).reshape(1, -1)
+        # note: the 4 zeroes are for missing categories=['INLAND', 'ISLAND',
+        # 'NEAR BAY','NEAR OCEAN']
+        # test with fake inputs
+        # fake = np.array([-122, 37, 40, 2000, 3000, 500, 3, 3, 6, 4, 0, 0,
+        # 1, 0]).reshape(1, -1)
         # std_fake = std_scaler.transform(fake)
-
-        # standardization
+        # Standardization
         std_inputs = std_scaler.transform(inputs)
 
         y = lin_reg.predict(std_inputs)
@@ -149,7 +157,5 @@ def make_prediction(clicks, longitude, latitude, housing_median_age, total_rooms
         return formatted_y
 
 
-
-############ Deploy
 if __name__ == '__main__':
     app.run_server(debug=True)
